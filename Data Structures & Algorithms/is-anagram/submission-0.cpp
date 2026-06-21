6469b56cd3cdd1be3661489fa99f4c836af22bdb
Vishal2501 <77535695+Vishal2501@users.noreply.github.com>
2026-06-21 12:16:38 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())   return false;
+        vector<int> vec(26, 0);
+        for (int i=0; i<s.length(); i++) {
+            vec[s[i]-'a']++;
+            vec[t[i]-'a']--;
+        }
+
+        for (int i=0; i<26; i++) {
+            if (vec[i] != 0)    return false;
+        }
+
+        return true;
+    }
+};
